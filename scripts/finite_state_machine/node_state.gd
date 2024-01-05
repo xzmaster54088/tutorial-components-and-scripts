e1274d35183f22ec189bb222927d820bd2ebc740
rapidvectors <rapidvectors@gmail.com>
2024-01-05 21:53:32 +0000

    Added the finite state machine scripts

---
+++ scripts/finite_state_machine/node_state.gd
@@ -0,0 +1,19 @@
+class_name NodeState
+extends Node
+
+signal transition
+
+func on_process(_delta : float):
+	pass
+
+
+func on_physics_process(_delta : float):
+	pass
+
+
+func on_enter():
+	pass
+
+
+func on_exit():
+	pass
